a534c14e7fe5270dc68157992d9e31e84c606e94
August Miller <augustamiller77@gmail.com>
2021-10-14 15:34:46 -0400

    Update UsableItem.java

--- src/main/java/Items/UsableItem.java
+++ src/main/java/Items/UsableItem.java
@@ -2,6 +2,10 @@ package Items;
 
 public abstract class UsableItem extends Item{
 
+    public int getUsesLeft() {
+        return usesLeft;
+    }
+
     //If -1, unlimited number of uses before destroying
     protected int usesLeft = -1;
 
